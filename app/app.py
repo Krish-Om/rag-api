2a8a99a0f289871b73fb097fdd92108ae2a5b092
Krish-Om <Krishombasukala@gmail.com>
2026-02-06 15:49:41 +0545

    feat: implement Redis connection management with async context in lifespan

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.ingestion import ingestion_router
+from app.database.redis_client import init_redis, close_redis
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up...")
+    await init_redis()
+    print("✅ Redis Connected")
+
+    yield
+
+    print(" Shutting down....")
+    await close_redis()
+
 
 app = FastAPI(
     title="RAG Api",
     version="1.0.0",
     description="APIs for Conversational, Document Ingestiontion and Processing",
+    lifespan=lifespan,
 )
 
 app.add_middleware(
